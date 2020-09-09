64351d3be0c1a315e5228ac3ac7035fe77fddf58
Paulo Santos <paulo@adianta.com.br>
2020-09-09 10:55:15 -0300

    Updates

---
+++ precache-manifest.a601b0ccb6b8ddff579f43de109e0e40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c83d99e431b6ae29e4e313ff04c271",
+    "url": "/gh-pages/index.html"
+  },
+  {
+    "revision": "56c12d63439cdb7f2390",
+    "url": "/gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6ccf16010ef82636aae9",
+    "url": "/gh-pages/static/js/2.f1d6c2d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages/static/js/2.f1d6c2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c12d63439cdb7f2390",
+    "url": "/gh-pages/static/js/main.6e686c2e.chunk.js"
+  },
+  {
+    "revision": "439c653badb4ced29648",
+    "url": "/gh-pages/static/js/runtime-main.1f8fb0c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
